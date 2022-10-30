18d11f9b3839a5864989bbbfb22bdb76394afcd3
George Satellite <lunaerxs@gmail.com>
2022-10-30 21:40:43 +0200

    pong progress

--- src/components/r3f/Canvas.tsx
+++ src/components/r3f/Canvas.tsx
@@ -1,74 +1,55 @@
-import { useEffect, useRef } from 'react'
-import { Canvas as R3FCanvas, useFrame } from '@react-three/fiber'
-import { Physics, Debug, usePlane, useSphere } from '@react-three/cannon'
+import { Canvas as R3FCanvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Physics, Debug, usePlane, useSphere, useBox } from '@react-three/cannon'
 import type { Triplet } from '@react-three/cannon'
 import type { Mesh } from 'three'
 
-const Plane = () => {
-	const [planePhysRef] = usePlane<Mesh>(() => ({
-		rotation: [-Math.PI / 2, 0, 0],
+const Paddle = () => {
+	const mainBoxArgs: Triplet = [1, 0.05, 1.2]
+	const [paddlePhysRef] = useBox<Mesh>(() => ({
+		type: 'Kinematic',
+		args: mainBoxArgs,
 	}))
-  return (
-    <mesh ref={planePhysRef}>
-      <planeGeometry args={[10, 10]} />
-    </mesh>
-  )
+
+	return (
+		<mesh ref={paddlePhysRef}>
+			<group>
+				<mesh>
+					<boxGeometry args={mainBoxArgs}/>
+					<meshStandardMaterial color='red'/>
+				</mesh>
+				<mesh position={[0, 0, 0.85]}>
+					<boxGeometry args={[0.25, 0.05, 0.5]}/>
+					<meshStandardMaterial color='blue'/>
+				</mesh>
+			</group>
+		</mesh>
+	)
 }
 
 const Ball = () => {
-  const [ballPhysRef, api] = useSphere<Mesh>(() => ({
-		mass: 1,
+	const radius = 0.2
+  const [spherePhysRef, physApi] = useSphere<Mesh>(() => ({
+		mass: 0.25,
+		args: [radius],
 		position: [0, 5, 0],
-		rotation: [0, 0, 0],
-		material: {
-			friction: 0.1,
-			restitution: 0.82
-		}
 	}))
-	const positionRef = useRef<Triplet>([0, 0, 0])
 
-	useEffect(() => {
-		const unsubscribe = api.position.subscribe(v => positionRef.current = v)
-		return unsubscribe
-	}, [api])
+	usePlane(() => ({
+    type: 'Static',
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -10, 0],
+    onCollide: () => {
+      physApi.position.set(0, 5, 0)
+      physApi.velocity.set(0, 5, 0)
+			physApi.rotation.set(0, 0, 0)
+    },
+  }))
 
-	useFrame(({ camera }) => {
-		const position = positionRef.current
-		camera.lookAt(...position)
-	})
-
-	useEffect(() => {
-		const onKeyDown = (event: KeyboardEvent) => {
-			const position = positionRef.current
-
-			switch (event.key) {
-				case 'ArrowLeft':
-					api.applyImpulse([-1,0,0], position)
-					break
-				case 'ArrowRight':
-					api.applyImpulse([1,0,0], position)
-					break
-				case 'ArrowDown':
-					api.applyImpulse([0,0,1], position)
-					break
-				case 'ArrowUp':
-					api.applyImpulse([0,0,-1], position)
-					break
-				case ' ':
-					api.applyImpulse([0,5,0], position)
-					break
-			}
-		}
-
-		addEventListener('keydown', onKeyDown)
-
-		return () => removeEventListener('keydown', onKeyDown)
-	}, [api])
-
-	return (
-    <mesh ref={ballPhysRef}>
-      <sphereGeometry />
-			<meshStandardMaterial color='#ff0000'/>
+  return (
+    <mesh castShadow ref={spherePhysRef}>
+      <sphereGeometry args={[radius, 32, 32]} />
+      <meshStandardMaterial/>
     </mesh>
   )
 }
@@ -76,12 +57,22 @@ const Ball = () => {
 export const Canvas = () => {
   return (
     <div className='fixed inset-0'>
-      <R3FCanvas camera={{ position: [-1, 5, 5] }}>
+      <R3FCanvas camera={{ position: [0, 5, 8] }}>
 				<ambientLight/>
-				<Physics>
+				<OrbitControls/>
+				<Physics
+					defaultContactMaterial={{
+						friction: 0.9,
+						restitution: 0.7,
+						contactEquationStiffness: 1e7,
+						contactEquationRelaxation: 1,
+						frictionEquationStiffness: 1e7,
+						frictionEquationRelaxation: 2,
+					}}
+				>
 					<Debug color='green' scale={1.1}>
-						<Plane/>
 						<Ball/>
+						<Paddle/>
 					</Debug>
 				</Physics>
 			</R3FCanvas>
