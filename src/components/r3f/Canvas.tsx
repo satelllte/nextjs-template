dc2292743cf731f9b6ae0f2e7ee9a6a635d7148b
George Satellite <lunaerxs@gmail.com>
2022-11-01 21:08:25 +0200

    pong: progress (2)

--- src/components/r3f/Canvas.tsx
+++ src/components/r3f/Canvas.tsx
@@ -1,15 +1,75 @@
-import { Canvas as R3FCanvas } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { Canvas as R3FCanvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug, usePlane, useSphere, useBox } from '@react-three/cannon'
 import type { Triplet } from '@react-three/cannon'
-import type { Mesh } from 'three'
+import { Mesh } from 'three'
+import {
+	ARROW_LEFT,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+} from '@/constants/keyboardEventCodes'
 
 const Paddle = () => {
 	const mainBoxArgs: Triplet = [1, 0.05, 1.2]
-	const [paddlePhysRef] = useBox<Mesh>(() => ({
+	const [paddlePhysRef, physApi] = useBox<Mesh>(() => ({
 		type: 'Kinematic',
 		args: mainBoxArgs,
 	}))
+
+	const positionRef = useRef<Triplet>([0, 0, 0])
+	const velocityRef = useRef<Triplet>([0, 0, 0])
+
+	useEffect(() => {
+		const unsubscribe = physApi.position.subscribe(position => positionRef.current = position)
+		return unsubscribe
+	}, [physApi])
+
+	useEffect(() => {
+		const unsubscribe = physApi.velocity.subscribe(velocity => velocityRef.current = velocity)
+		return unsubscribe
+	}, [physApi])
+
+	useEffect(() => {
+		const onKeyDown = (event: KeyboardEvent) => {
+			console.info('onKeyDown | event.code: ', event.code)
+
+			let inputX: number = 0
+			let inputY: number = 0
+
+			switch (event.code) {
+				case ARROW_LEFT:
+					inputX = -1
+					break
+				case ARROW_RIGHT:
+					inputX = 1
+					break
+				case ARROW_UP:
+					inputY = 1
+					break
+				case ARROW_DOWN:
+					inputY = -1
+					break
+			}
+
+			const [x, y, z] = velocityRef.current
+			const speed = 7.5
+			physApi.velocity.set(x + inputX * speed, y + inputY * speed, z)
+		}
+
+		addEventListener('keydown', onKeyDown)
+
+		return () => {
+			removeEventListener('keydown', onKeyDown)
+		}
+	}, [physApi])
+
+	useFrame(() => {
+		const damping = 0.5
+		const [x, y, z] = velocityRef.current
+		physApi.velocity.set(x * damping, y * damping, z * damping)
+	})
 
 	return (
 		<mesh ref={paddlePhysRef}>
---
+++ src/constants/keyboardEventCodes.ts
@@ -0,0 +1,4 @@
+export const ARROW_UP: string = 'ArrowUp'
+export const ARROW_DOWN: string = 'ArrowDown'
+export const ARROW_LEFT: string = 'ArrowLeft'
+export const ARROW_RIGHT: string = 'ArrowRight'
