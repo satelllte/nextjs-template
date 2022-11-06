cdac39b4ee3f3ed42a9e967c213701b015a74c6c
George Satellite <lunaerxs@gmail.com>
2022-11-06 15:03:39 +0200

    remove `resolveMetaTitle` helper

--- src/utils/storybook/resolveMetaTitle.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Resolves the story meta title string by given path
- * More info: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy
- */
- export const resolveMetaTitle = (...path: string[]): string => {
-	return path.join('/')
-}
