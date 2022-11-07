5fe1a3f5fce2364d1e42d38ab7b15077f2044441
George Satellite <lunaerxs@gmail.com>
2022-11-07 19:49:22 +0200

    radix: Accordion

---
+++ src/components/molecules/Accordion/Accordion.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Accordion } from './Accordion'
+import type { AccordionProps } from './types'
+
+export default {
+  title: 'molecules/Accordion',
+  component: Accordion,
+} as Meta<AccordionProps>
+
+export const Primary: StoryObj<AccordionProps> = {}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+			keyframes: {
+        ['radix-accordion-open']: {
+          'from': { height: 0 },
+          'to': { height: 'var(--radix-accordion-content-height)' },
+        },
+        ['radix-accordion-close']: {
+					'from': { height: 'var(--radix-accordion-content-height)' },
+          'to': { height: 0 },
+        },
+      },
+			animation: {
+				['radix-accordion-open']: 'radix-accordion-open 300ms ease-out',
+				['radix-accordion-close']: 'radix-accordion-close 300ms ease-out',
+			},
+		},
   },
   plugins: [],
 }
