11e6b806f8aafdab85a09ed43953c7b86ae8753b
George Satellite <lunaerxs@gmail.com>
2022-11-07 19:50:47 +0200

    tailwind config: remove redundant braces

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
   theme: {
     extend: {
 			keyframes: {
-        ['radix-accordion-open']: {
+        'radix-accordion-open': {
           'from': { height: 0 },
           'to': { height: 'var(--radix-accordion-content-height)' },
         },
-        ['radix-accordion-close']: {
+        'radix-accordion-close': {
 					'from': { height: 'var(--radix-accordion-content-height)' },
           'to': { height: 0 },
         },
       },
 			animation: {
-				['radix-accordion-open']: 'radix-accordion-open 300ms ease-out',
-				['radix-accordion-close']: 'radix-accordion-close 300ms ease-out',
+				'radix-accordion-open': 'radix-accordion-open 300ms ease-out',
+				'radix-accordion-close': 'radix-accordion-close 300ms ease-out',
 			},
 		},
   },
