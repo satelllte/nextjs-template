7f820d00392c3305f5ac041499ced7642fb4d2c9
George Satellite <lunaerxs@gmail.com>
2023-01-01 16:17:42 +0200

    remove storybook layout constants

--- src/constants/storybook/layout.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The `layout` parameter allows you to configure how stories are positioned in Storybook's Canvas tab.
- * Docs: https://storybook.js.org/docs/react/configure/story-layout
- */
- export enum StorybookLayout {
-	centered = 'centered',
-	fullscreen = 'fullscreen',
-	padded = 'padded'
-}
