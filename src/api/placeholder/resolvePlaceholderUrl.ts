440986751d56c11a566fe0983171004cab66b0ac
George Satellite <lunaerxs@gmail.com>
2023-07-24 21:13:25 +0300

    cleanup

--- src/api/placeholder/resolvePlaceholderUrl.ts
+++
@@ -1,9 +0,0 @@
-import { PLACEHOLDER_BASE_URL } from './constants'
-
-export const resolvePlaceholderUrl = (
-  width: number,
-  height: number,
-  color?: string
-) => {
-  return `${PLACEHOLDER_BASE_URL}/${width}x${height}${color ? `/${color}` : ''}`
-}
--- src/utils/rand/randInt.ts
+++
@@ -1,6 +0,0 @@
-export const randInt = (
-	min: number,
-	max: number,
-): number => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
